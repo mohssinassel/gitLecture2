22b1525edea3fe4c6171febac96e81bda6dbfdc2
mohssin <mohssinassalh@gmail.com>
2024-02-08 11:35:14 +0100

    new branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ public class Main {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome mohssin after pushing to remote");
-        System.out.printf("Hello and welcome mohssin after pushing ");
+        System.out.printf("Hello and welcome mohssin after pushing fe");
 
 
     }
